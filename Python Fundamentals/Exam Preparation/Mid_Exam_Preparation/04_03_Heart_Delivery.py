b3778fc32c6651cc000f670e57cb06435e95c500
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-04-13 17:00:58 +0200

    Update 04_03_Heart_Delivery.py

--- Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/04_03_Heart_Delivery.py
+++ Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/04_03_Heart_Delivery.py
@@ -1,43 +1,51 @@
-# neighborhood = list(map(int, input().split("@")))
-# commands = input()
-#
-# current_index_position = 0
-# cupids_last_position = 0
-# while True:
-#     command = commands.split()
-#
-#     if command[0] == "Love!":
-#         break
-#
-#     index = int(command[1]) + current_index_position
-#     if index >= len(neighborhood):
-#         index = 0
-#
-#     if -1 < index < len(neighborhood):
-#         if neighborhood[index] > 0:
-#             neighborhood[index] -= 2
-#             if neighborhood[index] == 0:
-#                 print(f"Place {index} has Valentine's day.")
-#         else:
-#             print(f"Place {index} already had Valentine's day.")
-#     cupids_last_position = index
-#     current_index_position = index
-#
-#     commands = input()
-#
-#
-# result = [x for x in neighborhood if x == 0]
-# print(f"Cupid's last position was {cupids_last_position}.")
-#
-# if len(result) != len(neighborhood):
-#     diff = abs(len(neighborhood) - len(result))
-#     print(f"Cupid has failed {diff} places.")
-# else:
-#     print("Mission was successful.")
+# First solution of the problem:
+
+neighborhood = list(map(int, input().split("@")))
+commands = input()
+
+current_index_position = 0
+cupids_last_position = 0
+while True:
+    command = commands.split()
+
+    if command[0] == "Love!":
+        break
+
+    index = int(command[1]) + current_index_position
+    if index >= len(neighborhood):
+        index = 0
+
+    if -1 < index < len(neighborhood):
+        if neighborhood[index] > 0:
+            neighborhood[index] -= 2
+            if neighborhood[index] == 0:
+                print(f"Place {index} has Valentine's day.")
+        else:
+            print(f"Place {index} already had Valentine's day.")
+    cupids_last_position = index
+    current_index_position = index
+
+    commands = input()
+
+
+result = [x for x in neighborhood if x == 0]
+print(f"Cupid's last position was {cupids_last_position}.")
+
+if len(result) != len(neighborhood):
+    diff = abs(len(neighborhood) - len(result))
+    print(f"Cupid has failed {diff} places.")
+else:
+    print("Mission was successful.")
 
 
 
 
+
+
+    
+
+    
+# Second solution of the problem:
 
 number_list = [int(num) for num in input().split("@")]
 current_position = 0
