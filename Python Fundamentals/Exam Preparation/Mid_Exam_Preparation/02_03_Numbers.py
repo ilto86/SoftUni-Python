8fafbe7ef7b53d6e6b7b24a83aec0e584edd82ce
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-04-13 16:47:22 +0200

    Update 02_03_Numbers.py

--- Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/02_03_Numbers.py
+++ Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/02_03_Numbers.py
@@ -1,58 +1,28 @@
-# numbers = [int(num) for num in input().split()]
-# result = sum(numbers)
-# avarage = result/ len(numbers)
-# avarage_5 = list()
-#
-# for i in numbers:
-#     if i > avarage:
-#         avarage_5.append(i)
-#
-# avarage_5.sort(reverse=True)
-# if len(avarage_5) > 5:
-#         avarage_5 = avarage_5[:5]
-# if len(avarage_5) > 0:
-#         output = [str(el) for el in avarage_5]
-#         print(" ".join(output))
-# else:
-#     print("No")
+# First solution of the problem:
+
+numbers = [int(el) for el in input().split()]
+
+greater_than_average = [n for n in numbers if n > sum(numbers) / len(numbers)]
+
+greater_than_average.sort(reverse=True)
+
+if len(greater_than_average) == 0:
+    print("No")
+
+elif len(greater_than_average) < 6:
+    greater_than_average = [str(n) for n in greater_than_average]
+    print(" ".join(greater_than_average))
+
+else:
+    greater_than_average = [str(n) for n in greater_than_average]
+    print(" ".join(greater_than_average[:5]))
+
+    
+ 
 
 
 
-
-# numbers = [int(num) for num in input().split()]
-# result = sum(numbers)
-# avarage = result/ len(numbers)
-# avarage_5 = [num for num in numbers if num > avarage]
-#
-# avarage_5.sort(reverse=True)
-#
-# if len(avarage_5) > 5:
-#         avarage_5 = avarage_5[:5]
-# if len(avarage_5) > 0:
-#         output = [str(el) for el in avarage_5]
-#         print(" ".join(output))
-# else:
-#     print("No")
-
-
-
-# numbers = [int(el) for el in input().split()]
-#
-# greater_than_average = [n for n in numbers if n > sum(numbers) / len(numbers)]
-#
-# greater_than_average.sort(reverse=True)
-#
-# if len(greater_than_average) == 0:
-#     print("No")
-#
-# elif len(greater_than_average) < 6:
-#     greater_than_average = [str(n) for n in greater_than_average]
-#     print(" ".join(greater_than_average))
-#
-# else:
-#     greater_than_average = [str(n) for n in greater_than_average]
-#     print(" ".join(greater_than_average[:5]))
-
+# Second solution of the problem:
 
 numbers = [int(num) for num in input().split()]
 avarage = sum(numbers) / len(numbers)
