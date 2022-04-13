d7904479c61f1df0b7d362ce78800a321946fa12
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-04-13 16:44:34 +0200

    Update 02_02_Array_Modifier.py

--- Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/02_02_Array_Modifier.py
+++ Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/02_02_Array_Modifier.py
@@ -1,24 +1,32 @@
-# array_values = [int(el) for el in input().split()]
-#
-# command = input()
-#
-# while command != "end":
-#     if command == "decrease":
-#         array_values = list(map(lambda x: x - 1, array_values))
-#     else:
-#         task = command.split()
-#         first_index = int(task[1])
-#         second_index = int(task[2])
-#         if task[0] == "swap":
-#             array_values[first_index], array_values[second_index] = array_values[second_index], array_values[first_index]
-#         elif task[0] == "multiply":
-#             array_values[first_index] = array_values[first_index] * array_values[second_index]
-#
-#     command = input()
-#
-# array_values = [str(el) for el in array_values]
-# print(", ".join(array_values))
+# First solution of the problem:
 
+array_values = [int(el) for el in input().split()]
+
+command = input()
+
+while command != "end":
+    if command == "decrease":
+        array_values = list(map(lambda x: x - 1, array_values))
+    else:
+        task = command.split()
+        first_index = int(task[1])
+        second_index = int(task[2])
+        if task[0] == "swap":
+            array_values[first_index], array_values[second_index] = array_values[second_index], array_values[first_index]
+        elif task[0] == "multiply":
+            array_values[first_index] = array_values[first_index] * array_values[second_index]
+
+    command = input()
+
+array_values = [str(el) for el in array_values]
+print(", ".join(array_values))
+
+
+
+
+
+
+# Second solution of the problem:
 
 array_values = [int(num) for num in input().split()]
 
