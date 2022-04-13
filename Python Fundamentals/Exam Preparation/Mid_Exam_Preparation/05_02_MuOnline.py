cfbaeb138d7092564de6213784865e8b4eb1ddf0
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-04-13 17:02:32 +0200

    Update 05_02_MuOnline.py

--- Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/05_02_MuOnline.py
+++ Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/05_02_MuOnline.py
@@ -1,48 +1,54 @@
-# health = 100
-# bitcoins = 0
-# room = 0
-#
-# command = input().split("|")
-# dead = False
-#
-# for current in command:
-#     current_command = current.split()[0]
-#     current_number = int(current.split()[1])
-#     room += 1
-#
-#     if current_command == "potion":
-#         if health + current_number > 100:
-#             amount = 100 - health
-#             health = 100
-#         else:
-#             health += current_number
-#             amount = current_number
-#         print(f"You healed for {amount} hp.")
-#         print(f"Current health: {health} hp.")
-#
-#     elif current_command == "chest":
-#         bitcoins += current_number
-#         print(f"You found {current_number} bitcoins.")
-#
-#     else:
-#         health -= current_number
-#         if health > 0:
-#             print(f"You slayed {current_command}.")
-#         else:
-#             dead = True
-#             print(f"You died! Killed by {current_command}.")
-#             print(f"Best room: {room}")
-#
-#     if dead:
-#         break
-#
-# if not dead:
-#     print("You've made it!")
-#     print(f"Bitcoins: {bitcoins}")
-#     print(f"Health: {health}")
+# First solution of the problem:
+
+health = 100
+bitcoins = 0
+room = 0
+
+command = input().split("|")
+dead = False
+
+for current in command:
+    current_command = current.split()[0]
+    current_number = int(current.split()[1])
+    room += 1
+
+    if current_command == "potion":
+        if health + current_number > 100:
+            amount = 100 - health
+            health = 100
+        else:
+            health += current_number
+            amount = current_number
+        print(f"You healed for {amount} hp.")
+        print(f"Current health: {health} hp.")
+
+    elif current_command == "chest":
+        bitcoins += current_number
+        print(f"You found {current_number} bitcoins.")
+
+    else:
+        health -= current_number
+        if health > 0:
+            print(f"You slayed {current_command}.")
+        else:
+            dead = True
+            print(f"You died! Killed by {current_command}.")
+            print(f"Best room: {room}")
+
+    if dead:
+        break
+
+if not dead:
+    print("You've made it!")
+    print(f"Bitcoins: {bitcoins}")
+    print(f"Health: {health}")
 
 
 
+
+    
+        
+# Second solution of the problem:
 
 health = 100
 bitcoins = 0
