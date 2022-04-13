7e7ba354aa3e7ccde918055ca1c7b45aa2a86707
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-04-13 16:55:03 +0200

    Update 03_02_Shoot_for_the_Win.py

--- Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/03_02_Shoot_for_the_Win.py
+++ Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/03_02_Shoot_for_the_Win.py
@@ -1,58 +1,73 @@
-# numbers = [int(num) for num in input().split()]
-# command = input()
-# shoot_a_targets = 0
-# substract = 0
-# collect = 0
-# target_num = 0
-# index = 0
-#
-# while command != "End":
-#     targets = int(command)
-#     if targets > len(numbers) - 1:
-#         command = input()
-#         continue
-#     target_num = numbers[targets]
-#     index = 0
-#     for num in numbers:
-#         if num > numbers[targets] and numbers[targets] != -1:
-#             substract = num - numbers[targets]
-#             numbers[index] = substract
-#         elif 0 < num <= numbers[targets] and numbers[targets] != -1:
-#             if targets == index:
-#                 index += 1
-#                 continue
-#             collect = num + numbers[targets]
-#             numbers[index] = collect
-#
-#         index += 1
-#         continue
-#
-#     if numbers[targets] == target_num:
-#         target_num = -1
-#         shoot_a_targets += 1
-#         numbers[targets] = target_num
-#
-#     command = input()
-# numbers = [str(num) for num in numbers]
-# output = " ".join(numbers)
-# print(f"Shot targets: {shoot_a_targets} -> {output}" )
+# First solution of the problem:
+
+numbers = [int(num) for num in input().split()]
+command = input()
+shoot_a_targets = 0
+substract = 0
+collect = 0
+target_num = 0
+index = 0
+
+while command != "End":
+    targets = int(command)
+    
+    if targets > len(numbers) - 1:
+        command = input()
+        continue
+    
+    target_num = numbers[targets]
+    index = 0
+    
+    for num in numbers:
+        
+        if num > numbers[targets] and numbers[targets] != -1:
+            substract = num - numbers[targets]
+            numbers[index] = substract
+        
+        elif 0 < num <= numbers[targets] and numbers[targets] != -1:
+            
+            if targets == index:
+                index += 1
+                continue
+            
+            collect = num + numbers[targets]
+            numbers[index] = collect
+
+        index += 1
+        continue
+
+    if numbers[targets] == target_num:
+        target_num = -1
+        shoot_a_targets += 1
+        numbers[targets] = target_num
+
+    command = input()
+
+numbers = [str(num) for num in numbers]
+output = " ".join(numbers)
+print(f"Shot targets: {shoot_a_targets} -> {output}" )
 
 
 
 
+
+
+# Second solution of the problem:
 
 numbers = [int(num) for num in input().split()]
 count = 0
 while True:
     command = input()
+    
     if command == "End":
         break
 
     index = int(command)
+    
     if 0 <= index < len(numbers):
-
         targets_num = numbers[index]
         count += 1
+        
         for idx, num in enumerate(numbers):
 
             if targets_num > 0 and idx != index and num != -1:
