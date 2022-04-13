4e499a1b04be1134300ba2a7ae82bea25c91f646
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-04-13 16:42:29 +0200

    Update 02_01_SoftUni_Reception.py

--- Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/02_01_SoftUni_Reception.py
+++ Python Fundamentals/Exam Preparation/Mid_Exam_Preparation/02_01_SoftUni_Reception.py
@@ -1,24 +1,3 @@
-# first_employee_efficiency = int(input())
-# second_employee_efficiency = int(input())
-# third_employee_efficiency = int(input())
-# students_count = int(input())
-# time_count = 0
-#
-# per_hour = first_employee_efficiency + second_employee_efficiency + third_employee_efficiency
-#
-# while students_count > 0:
-#     time_count += 1
-#
-#     if time_count % 4 == 0:
-#         continue
-#
-#     students_count -= per_hour
-#
-# print(f"Time needed: {time_count}h.")
-
-
-
-
 first = int(input())
 second = int(input())
 third = int(input())
@@ -36,5 +15,3 @@ while True:
     else:
         students_count -= all_employees_efficiency
 print(f"Time needed: {time}h.")
-
-
