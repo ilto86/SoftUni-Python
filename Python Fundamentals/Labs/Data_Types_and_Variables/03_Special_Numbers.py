07490bde49cda9ebeb9b05ba89921eda7bae3073
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 13:47:25 +0200

    Update 03_Special_Numbers.py

--- Python Fundamentals/Labs/Data_Types_and_Variables/03_Special_Numbers.py
+++ Python Fundamentals/Labs/Data_Types_and_Variables/03_Special_Numbers.py
@@ -1,16 +1,3 @@
-n = int(input())
-
-for num in range(1, n + 1):
-    nums_as_string = str(num)
-    result = 0
-    for symbol in nums_as_string:
-        result += int(symbol)
-    if result == 5 or result == 7 or result == 11:
-        print(f"{num} -> True")
-    else:
-        print(f"{num} -> False")
-#
-#
 # n = int(input())
 #
 # for num in range(1, n + 1):
@@ -25,15 +12,15 @@ for num in range(1, n + 1):
 #         print(f"{num} -> False")
 
 
-# number = int(input())
-#
-# for n in range(1, number + 1):
-#     working_number = n
-#     digit_sum = 0
-#
-#     while working_number > 0:
-#         digit_sum += working_number % 10
-#         working_number = int(working_number / 10)
-#
-#     is_special = digit_sum == 5 or digit_sum == 7 or digit_sum == 11
-#     print(f"{n} -> {is_special}")
+
+n = int(input())
+
+for num in range(1, n + 1):
+    nums_as_string = str(num)
+    result = 0
+    for symbol in nums_as_string:
+        result += int(symbol)
+    if result == 5 or result == 7 or result == 11:
+        print(f"{num} -> True")
+    else:
+        print(f"{num} -> False")
