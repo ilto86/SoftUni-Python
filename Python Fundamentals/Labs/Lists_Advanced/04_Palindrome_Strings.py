8f6b71413518ae928430a9a4ee6b22a2b8eed44d
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 14:03:07 +0200

    Update 04_Palindrome_Strings.py

--- Python Fundamentals/Labs/Lists_Advanced/04_Palindrome_Strings.py
+++ Python Fundamentals/Labs/Lists_Advanced/04_Palindrome_Strings.py
@@ -13,30 +13,3 @@ for word in words:
 print(palindrome_words)
 palindrome_count = words.count(palindrome)
 print(f"Found palindrome {palindrome_count} times")
-
-
-
-
-
-# words = input().split()
-# palindrome_words = list()
-# palindrome = input()
-#
-#
-# for word in words:
-#     rev2_list = list()
-#     for ch in word:
-#         rev2_list.append(ch)
-#     rev2_list.reverse()
-#
-#     rev2_word = "".join(rev2_list)
-#     if rev2_word == word:
-#         palindrome_words.append(word)
-#
-#
-#
-# print(palindrome_words)
-# palindrome_count = words.count(palindrome)
-# print(f"Found palindrome {palindrome_count} times")
-
-
