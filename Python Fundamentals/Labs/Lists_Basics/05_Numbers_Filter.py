a2039f618f9877e1d7ac738ea74c0dea2dde61fc
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 14:24:50 +0200

    Update 05_Numbers_Filter.py

--- Python Fundamentals/Labs/Lists_Basics/05_Numbers_Filter.py
+++ Python Fundamentals/Labs/Lists_Basics/05_Numbers_Filter.py
@@ -1,3 +1,7 @@
+'''
+First Solution
+'''
+
 # n = int(input())
 #
 # even_nums = []
@@ -29,74 +33,9 @@
 #     print(negative_nums)
 
 
-
-
-# n = int(input())
-# numbers = []
-# filtered = []
-#
-# for _ in range(n):
-#     current_number = int(input())
-#     numbers.append(current_number)
-#
-# command = input()
-# if command == "even":
-#     for number in numbers:
-#         if number % 2 == 0:
-#             filtered.append(number)
-# elif command == "odd":
-#     for number in numbers:
-#         if number % 2 != 0:
-#             filtered.append(number)
-# elif command == "positive":
-#     for number in numbers:
-#         if number >= 0:
-#             filtered.append(number)
-# elif command == "negative":
-#     for number in numbers:
-#         if number < 0:
-#             filtered.append(number)
-# print(filtered)
-
-
-
-
-
-# n = int(input())
-# all_nums = []
-#
-# even_nums = []
-# odd_nums = []
-# positive_nums = []
-# negative_nums = []
-#
-# for _ in range(n):
-#     current_num = int(input())
-#     all_nums.append(current_num)
-#
-# command = input()
-# if command == "even":
-#     for num in all_nums:
-#         if num % 2 == 0:
-#             even_nums.append(num)
-#     print(even_nums)
-# elif command == "positive":
-#     for num in all_nums:
-#         if num >= 0:
-#             positive_nums.append(num)
-#     print(positive_nums)
-# elif command == "odd":
-#     for num in all_nums:
-#         if num % 2 == 1:
-#             odd_nums.append(num)
-#     print(odd_nums)
-# else:
-#     for num in all_nums:
-#         if num < 0:
-#             negative_nums.append(num)
-#     print(negative_nums)
-
-
+'''
+Second Solution
+'''
 
 # number = int(input())
 #
@@ -125,6 +64,9 @@
 # print(filtered)
 
 
+'''
+Thirt Solution
+'''
 
 number = int(input())
 directed_commands = ["even", "odd", "positive", "negative"]
@@ -154,11 +96,3 @@ else:
     print(f"Negative : {negative}")
     print(f"Even : {even}")
     print(f"ODD : {odd}")
-# if filter_word == "even":
-#     print(even)
-# if filter_word == "odd":
-#     print(odd)
-# if filter_word == "positive":
-#     print(positive)
-# if filter_word == "negative":
-#     print(negative)
