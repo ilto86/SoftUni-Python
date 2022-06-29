49066f1d7aa458db3635c402ec1eb9c0623d9a42
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 13:54:11 +0200

    Update 07_Rounding.py

--- Python Fundamentals/Labs/Functions/07_Rounding.py
+++ Python Fundamentals/Labs/Functions/07_Rounding.py
@@ -1,28 +1,16 @@
-# def convert_list_to_round(base_list):
-#     final_list = list()
-#     for n in base_list:
-#         final_n = round(float(n))
-#         final_list.append(final_n)
-#
-#     return final_list
-#
-# input_list = input().split(" ")
-#
-# print(convert_list_to_round(input_list))
-
-
-
 # def convert_list_to_round(base_list, num_digits = 0):
 #     final_list = list()
 #     for n in base_list:
 #         final_n = round(float(n), num_digits)
 #         final_list.append(final_n)
-#
+
 #     return final_list
-#
+
 # input_list = input().split(" ")
 # current_round = int(input())
 # print(convert_list_to_round(input_list, current_round))
+
+
 
 
 def convert_list_to_round(base_list):
