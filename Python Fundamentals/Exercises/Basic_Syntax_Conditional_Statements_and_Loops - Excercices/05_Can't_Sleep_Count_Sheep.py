9e8c5ea3d92054b9afef1a1899899bd618d3505f
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 14:27:54 +0200

    Update 05_Can't_Sleep_Count_Sheep.py

--- Python Fundamentals/Exercises/Basic_Syntax_Conditional_Statements_and_Loops - Excercices/05_Can't_Sleep_Count_Sheep.py
+++ Python Fundamentals/Exercises/Basic_Syntax_Conditional_Statements_and_Loops - Excercices/05_Can't_Sleep_Count_Sheep.py
@@ -1,13 +1,26 @@
+'''
+First Solution
+'''
+
 # number = int(input())
 # count = 0
 # for i in range(number):
 #     count += 1
 #     print(f"{count} sheep...", end= '')
 
+
+'''
+Second Solution
+'''
+
 # total_sheeps = int(input())
 # for number_of_sheeps in range(1, total_sheeps + 1):
 #     print(f"{number_of_sheeps} sheep...", end = "")
 
+
+'''
+Thirt Solution
+'''
 
 number = int(input())
 
