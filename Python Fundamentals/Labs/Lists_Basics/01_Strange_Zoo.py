160815d88681c819323a69a4aeaceb4acaa10f2a
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 14:17:37 +0200

    Update 01_Strange_Zoo.py

--- Python Fundamentals/Labs/Lists_Basics/01_Strange_Zoo.py
+++ Python Fundamentals/Labs/Lists_Basics/01_Strange_Zoo.py
@@ -1,24 +1,36 @@
+'''
+First Solution
+'''
+
+# meerkat = []
+
+# meerkat.append(input())
+# meerkat.append(input())
+# meerkat.append(input())
+
+# meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
+# print(meerkat)
+
+'''
+Second Solution
+'''
+
 # tail = input()
 # body = input()
 # head = input()
-#
-# zoo = [head, body, tail]
-# print(zoo)
 
-
-# tail = input()
-# body = input()
-# head = input()
-#
 # zoo = [tail, body, head]
 # zoo[0], zoo[2] = zoo[2], zoo[0]
 # print(zoo)
 
-meerkat = []
 
-meerkat.append(input())
-meerkat.append(input())
-meerkat.append(input())
+'''
+Thirt Solution
+'''
 
-meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
-print(meerkat)
+tail = input()
+body = input()
+head = input()
+
+zoo = [head, body, tail]
+print(zoo)
