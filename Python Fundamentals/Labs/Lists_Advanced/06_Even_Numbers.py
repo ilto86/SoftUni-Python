452cc36048b062fe90d61f3f5b1ab7d4a232897c
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2022-06-29 14:13:45 +0200

    Update 06_Even_Numbers.py

--- Python Fundamentals/Labs/Lists_Advanced/06_Even_Numbers.py
+++ Python Fundamentals/Labs/Lists_Advanced/06_Even_Numbers.py
@@ -1,3 +1,20 @@
+'''
+First Solution with List Comprehension but this is not readable
+'''
+# print([index for index in range(len(nums)) if nums[index] % 2 == 0, int(el) for el in input().split(", ") ])
+
+'''
+Second Solution with List Comprehension this is more readable from first one
+'''
+
+# nums=[int(el) for el in input().split(", ")]
+# print([index for index in range(len(nums)) if nums[index] % 2 == 0])
+
+
+'''
+Thirt Solution without List Comprehension and this is more readable than the other two
+'''
+
 numbers = input().split(", ")
 numbers = list(map(int, numbers))
 even_index_numbers = list()
@@ -9,11 +26,3 @@ for i in range(len(numbers)):
         even_index_numbers.append(i)
 
 print(even_index_numbers)
-
-
-
-# nums=[int(el) for el in input().split(", ")]
-# print([index for index in range(len(nums)) if nums[index]%2==0])
-
-# nums=[int(el) for el in input().split(", ")]
-# print([index for index in range(len(nums)) if nums[index] % 2 == 0, int(el) for el in input().split(", ") ])
